c2b4ebeecd453c510d8bd64461fecaeefeb3f41e
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-07-25 01:16:43 -1100

    feat: mongoDB database setup and connected

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import http from "http";
 import app from "./app.js";
+import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
+
+// connect to mongoDB
+connectDB();
 
 server.listen(PORT, () => {
   console.log(`server listening on port ${PORT}`);
