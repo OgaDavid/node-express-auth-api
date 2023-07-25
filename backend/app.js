eccabac1dc2f7b8260be1c605039e0756f3f421e
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-07-25 01:35:55 -1100

    feat: created endpoint to register user and save to database

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,9 @@ import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api/v1/users", userRoutes);
 
 app.get("/", (req, res) => {
