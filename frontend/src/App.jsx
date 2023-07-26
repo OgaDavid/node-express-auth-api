72d41926dfc7c2d6f0bc87de881aa48f12365e01
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-07-25 15:23:24 -1100

    feat: frontend/ added react router dom, created homescreen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
+import { Container } from "react-bootstrap";
 import Header from "./components/Header";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-    <Header />
-      <div>Node + Express Auth API</div>
+      <Header />
+      <Container className="my-2">
+        <Outlet />
+      </Container>
     </>
   );
 };
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,23 +6,23 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-            <Navbar.Brand href='/'>
-                <img src="vite.svg" alt='logo' />
-                NODE AUTH
-            </Navbar.Brand>
+          <Navbar.Brand href="/">
+            <img src="vite.svg" alt="logo" />
+            NODE AUTH
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-                  <Nav.Link href="/login">
-                    <Nav.Link>
-                      <FaSignInAlt /> Sign In
-                    </Nav.Link>
-                  </Nav.Link>
-                  <Nav.Link href="/register">
-                    <Nav.Link>
-                      <FaSignOutAlt /> Sign Up
-                    </Nav.Link>
-                  </Nav.Link>
+              <Nav.Link href="/login">
+                <Nav.Link>
+                  Sign In
+                </Nav.Link>
+              </Nav.Link>
+              <Nav.Link href="/register">
+                <Nav.Link>
+                  Sign Up
+                </Nav.Link>
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
