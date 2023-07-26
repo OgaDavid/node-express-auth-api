ed74df3ecde0a0d77e0b6b8810e8679c5d7c6e6a
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-07-25 15:55:26 -1100

    feat: created register screen, routes correctly.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,11 +10,15 @@ import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import HomeScreen from "./pages/HomeScreen.jsx";
+import LoginScreen from "./pages/LoginScreen.jsx";
+import RegisterScreen from "./pages/RegisterScreen.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="/" index={true} element={<HomeScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
     </Route>
   )
 );
